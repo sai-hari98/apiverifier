66c74162ccf52f74072f89c4e82ac542314f42ea
Sairam Harikrishnan <sai_hari98@hotmail.com>
2024-03-14 07:54:37 -0500

    added README and Springboot app

---
+++ src/main/java/com/saihari98/apiverifier/ApiverifierApplication.java
@@ -0,0 +1,13 @@
+package com.saihari98.apiverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiverifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiverifierApplication.class, args);
+	}
+
+}
